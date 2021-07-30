75ffaf62ca85369ada40e16f5ccc06b3045376a6
vikashca <vikkychauhanca@gmail.com>
2021-07-30 19:14:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HomePage from './modules/home/home-page.component';
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
