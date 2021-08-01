9e829a68d1ef6b181eaad904ea1c6d7d5e0ef5bb
vikashca <vikkychauhanca@gmail.com>
2021-08-01 12:34:49 +0530

    update our menu-items with routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom'
 import HomePage from './pages/home-page/home-page.component';
+
+const HatsPage = (props) => {
+
+  console.log(props.match.params)
+  return (
+  <div>
+    <h1>Home PAGE</h1>
+  </div>
+  );
+ };
+
+const TopicList = (props) => {
+  return (
+  <div>
+    <h1>Topic List</h1>
+    <Link to={`${props.match.url}/13`} >To topic 13</Link>
+    <Link to={`${props.match.url}/14`} >To topic 14</Link>
+    <Link to={`${props.match.url}/15`} >To topic 15</Link>
+
+  </div>
+  );
+}
+
+const TopicDetail = (props) => {
+
+  console.log(props)
+
+  return (
+  <div>
+    <h1>Topic Detail PAGE</h1>
+  </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/'  component={HomePage}/>
+        <Route exact path='/topics'  component={TopicList}/>
+        <Route  path='/topics/:topicId'  component={TopicDetail}/>
+
+      </Switch>
+      {/* <HomePage/> */}
     </div>
   );
 }
--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -3,7 +3,7 @@ import Directory from '../../components/directory/directory.component'
 import './home-page.style.scss';
 
 
-const HomePage = () => (
+const HomePage = ( ) => (
   <div className="home-page">
     <Directory/>  
   </div>
