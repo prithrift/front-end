80e7b4569b7ecefc30e27b8e8881ff963b237e43
vikashca <vikkychauhanca@gmail.com>
2021-08-01 15:16:08 +0530

    shoping-Card-styling

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
 import { Switch, Route, Link } from 'react-router-dom'
 import HomePage from './pages/home-page/home-page.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = (props) => {
+import './App.css';
 
-  console.log(props.match.params)
-  return (
-  <div>
-    <h1>Home PAGE</h1>
-  </div>
-  );
- };
-
-const TopicList = (props) => {
-  return (
-  <div>
-    <h1>Topic List</h1>
-    <Link to={`${props.match.url}/13`} >To topic 13</Link>
-    <Link to={`${props.match.url}/14`} >To topic 14</Link>
-    <Link to={`${props.match.url}/15`} >To topic 15</Link>
-
-  </div>
-  );
-}
-
-const TopicDetail = (props) => {
-
-  console.log(props)
-
-  return (
-  <div>
-    <h1>Topic Detail PAGE</h1>
-  </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/'  component={HomePage}/>
-        <Route exact path='/topics'  component={TopicList}/>
-        <Route  path='/topics/:topicId'  component={TopicDetail}/>
-
+        <Route exact path='/shop'  component={ShopPage}/>
+        {/* <Route  path='/topics/:topicId'  component={TopicDetail}/> */}
       </Switch>
-      {/* <HomePage/> */}
+
     </div>
   );
 }
 
 export default App;
+
+// const HatsPage = (props) => {
+
+//   console.log(props.match.params)
+//   return (
+//   <div>
+//     <h1>Home PAGE</h1>
+//   </div>
+//   );
+//  };
+
+// const TopicList = (props) => {
+//   return (
+//   <div>
+//     <h1>Topic List</h1>
+//     <Link to={`${props.match.url}/13`} >To topic 13</Link>
+//     <Link to={`${props.match.url}/14`} >To topic 14</Link>
+//     <Link to={`${props.match.url}/15`} >To topic 15</Link>
+
+//   </div>
+//   );
+// }
+
+// const TopicDetail = (props) => {
+
+//   console.log(props)
+
+//   return (
+//   <div>
+//     <h1>Topic Detail PAGE</h1>
+//   </div>
+//   );
+// }
